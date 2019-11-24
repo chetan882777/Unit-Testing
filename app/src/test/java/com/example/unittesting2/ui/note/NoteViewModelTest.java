1af48298fd9efb9f4135e04654a8492a4424bcd3
chetan882777 <chetan882777@gmail.com>
2019-11-24 13:17:49 -0800

    NoteViewModel Unit Tests (part 1)

---
+++ app/src/test/java/com/example/unittesting2/ui/note/NoteViewModelTest.java
@@ -0,0 +1,136 @@
+package com.example.unittesting2.ui.note;
+
+import com.example.unittesting2.models.Note;
+import com.example.unittesting2.repository.NoteRepository;
+import com.example.unittesting2.ui.Resource;
+import com.example.unittesting2.util.LiveDataTestUtil;
+import com.example.unittesting2.util.TestUtil;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import io.reactivex.Flowable;
+import io.reactivex.internal.operators.single.SingleToFlowable;
+
+import static com.example.unittesting2.repository.NoteRepository.INSERT_SUCCESS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.never;
+
+public class NoteViewModelTest {
+
+    // system under test
+    private NoteViewModel noteViewModel;
+
+    @Mock
+    private NoteRepository noteRepository;
+
+    @BeforeEach
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+        noteViewModel = new NoteViewModel(noteRepository);
+    }
+
+    /*
+        can't observe a note that hasn't been set
+     */
+
+    @Test
+    void observeEmptyNoteWhenNoteSet() throws Exception {
+        // Arrange
+        LiveDataTestUtil<Note> liveDataTestUtil = new LiveDataTestUtil<>();
+
+        // Act
+        Note note = liveDataTestUtil.getValue(noteViewModel.observeNote());
+
+        // Assert
+        assertNull(note);
+    }
+
+    /*
+        Observe a note has been set and onChanged will trigger in activity
+     */
+
+    @Test
+    void observeNote_whenSet() throws Exception {
+
+        // Arrange
+        Note note = new Note(TestUtil.TEST_NOTE_1);
+        LiveDataTestUtil<Note> liveDataTestUtil = new LiveDataTestUtil<>();
+
+        // Act
+        noteViewModel.setNote(note);
+        Note observedNote = liveDataTestUtil.getValue(noteViewModel.observeNote());
+
+        // Assert
+        assertEquals(note, observedNote);
+    }
+
+    /*
+        Insert a new note and observe row returned
+     */
+
+    @Test
+    void insertNote_returnRow() throws Exception {
+        // Arrange
+        Note note = new Note(TestUtil.TEST_NOTE_1);
+        LiveDataTestUtil<Resource<Integer>> liveDataTestUtil = new LiveDataTestUtil<>();
+        final int insertedRow = 1;
+        Flowable<Resource<Integer>> returnedData = SingleToFlowable.just(Resource.success(insertedRow, INSERT_SUCCESS));
+        when(noteRepository.insertNote(any(Note.class))).thenReturn(returnedData);
+
+        // Act
+        noteViewModel.setNote(note);
+        Resource<Integer> returnedValue = liveDataTestUtil.getValue(noteViewModel.insertNote());
+
+        // Assert
+        assertEquals(Resource.success(insertedRow, INSERT_SUCCESS), returnedValue);
+    }
+
+    /*
+        insert: dont return a new row without observer
+     */
+
+    @Test
+    void dontReturnInsertRowWithoutObserver() throws Exception {
+
+        // Arrange
+        Note note = new Note(TestUtil.TEST_NOTE_1);
+
+        // Act
+        noteViewModel.setNote(note);
+
+        // Assert
+        verify(noteRepository, never()).insertNote(any(Note.class));
+    }
+
+    /*
+        set note, null title, throw exception
+     */
+
+    @Test
+    void setNote_nullTitle_throwException() throws Exception {
+
+        // Arrange
+        final Note note = new Note(TestUtil.TEST_NOTE_1);
+        note.setTitle(null);
+
+        // Assert
+        assertThrows(Exception.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+
+                // Act
+                noteViewModel.setNote(note);
+            }
+        });
+    }
+
+}
